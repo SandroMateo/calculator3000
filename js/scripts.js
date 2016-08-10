d1679540d0379b44ac824e8f2ca08d6ff2ecb058
Sandro Alvarez and Satchel Grant <satchel@>
2016-08-10 11:01:07 -0700

    added buttons to execute math using js

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,16 @@ var add = function(number1, number2) {
 	return number1 + number2;
 };
 var subtract = function(number1, number2) {
-  return number1 - number2;
+	return number1 - number2;
 };
 var multiply = function(number1, number2) {
-  return number1 * number2;
+	return number1 * number2;
 };
 var divide = function(number1, number2) {
-  return number1 / number2;
+	return number1 / number2;
 };
 var remainder = function(number1, number2) {
-  return number1 % number2;
+	return number1 % number2;
 };
 var BMI = function(weight, height) {
 	return weight / height;
@@ -28,3 +28,47 @@ var converterG2L = function(gallons) {
 var converterL2G = function(liters) {
 	return liters / 3.78541;
 };
+
+$(function() {
+
+	$(".btn-success").click(function(event) {
+		var number1 = parseInt($("#num1").val());
+		var number2 = parseInt($("#num2").val());
+		var output = add(number1,number2);
+
+		$("#output").prepend("<li>" + output + "</li>");
+
+		event.preventDefault();
+	});
+
+	$(".btn-warning").click(function(event) {
+		var number1 = parseInt($("#num1").val());
+		var number2 = parseInt($("#num2").val());
+		var output = subtract(number1,number2);
+
+		$("#output").prepend("<li>" + output + "</li>");
+
+		event.preventDefault();
+	});
+
+	$(".btn-primary").click(function(event) {
+		var number1 = parseInt($("#num1").val());
+		var number2 = parseInt($("#num2").val());
+		var output = multiply(number1,number2);
+
+		$("#output").prepend("<li>" + output + "</li>");
+
+		event.preventDefault();
+	});
+
+	$(".btn-danger").click(function(event) {
+		var number1 = parseInt($("#num1").val());
+		var number2 = parseInt($("#num2").val());
+		var output = divide(number1,number2);
+		var modulo = remainder(number1, number2);
+
+		$("#output").prepend("<li>" + Math.floor(output) + " r" + modulo + "</li>");
+
+		event.preventDefault();
+	});
+});
