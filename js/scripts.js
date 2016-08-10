75ef225560413ee2149ada7df76413eb4c7cf543
Sandro Alvarez and Satchel Grant <satchel@>
2016-08-10 13:47:28 -0700

    add converter functionality

--- js/scripts.js
+++ js/scripts.js
@@ -30,45 +30,55 @@ var converterL2G = function(liters) {
 };
 
 $(function() {
-
-	$(".btn-success").click(function(event) {
+	// calculator functions
+	$("#add-btn").click(function(event) {
 		var number1 = parseInt($("#num1").val());
 		var number2 = parseInt($("#num2").val());
 		var output = add(number1,number2);
-
-		$("#output").prepend("<li>" + output + "</li>");
-
+		$("#calculator-output").text(output);
 		event.preventDefault();
 	});
-
 	$(".btn-warning").click(function(event) {
 		var number1 = parseInt($("#num1").val());
 		var number2 = parseInt($("#num2").val());
 		var output = subtract(number1,number2);
-
-		$("#output").prepend("<li>" + output + "</li>");
-
+		$("#calculator-output").text(output);
 		event.preventDefault();
 	});
-
 	$(".btn-primary").click(function(event) {
 		var number1 = parseInt($("#num1").val());
 		var number2 = parseInt($("#num2").val());
 		var output = multiply(number1,number2);
-
-		$("#output").prepend("<li>" + output + "</li>");
-
+		$("#calculator-output").text(output);
 		event.preventDefault();
 	});
-
 	$(".btn-danger").click(function(event) {
 		var number1 = parseInt($("#num1").val());
 		var number2 = parseInt($("#num2").val());
 		var output = divide(number1,number2);
 		var modulo = remainder(number1, number2);
-
-		$("#output").prepend("<li>" + Math.floor(output) + " r" + modulo + "</li>");
-
+		$("#calculator-output").text(Math.floor(output) + " r" + modulo);
 		event.preventDefault();
 	});
+	// converter functions
+	$("#converter-btn").click(function(event) {
+    var quantity = parseInt($("#input1").val());
+    var unit = $("#input2").val();
+    var output = 0;
+    if(unit === "gal") {
+      output = converterG2L(quantity);
+      unit = "L";
+    } else if (unit === "L") {
+      output = converterL2G(quantity);
+      unit = "gal";
+    } else if (unit === "C") {
+      output = tempC2F(quantity);
+      unit = "F";
+    } else {
+      output = tempF2C(quantity);
+      unit = "C";
+    }
+    $("#converter-output").text(output.toFixed(4) + " " + unit);
+    event.preventDefault();
+  });
 });
